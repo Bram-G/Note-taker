12dfec91205acc3f0de6a6cbbf4726c89f8746e7
Bram Gibson <Bram.gibson@hotmail.com>
2023-01-31 22:55:24 -0800

    reverting

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || "3000";
-const path = require("path");
-const fs = require("fs");
-const routes = require("./controllers/indexcontroller");
+const PORT = process.env.PORT || "3000"
+const path = require('path');
+const fs = require("fs")
+const apiRoutes = require('./controllers/apiController.js')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.use("/", routes);
+app.use('/api/notes',apiRoutes)
 
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "./index.html"))
-);
+
+app.get('/notes',(req,res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+
+app.get('*',(req,res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html') ));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
