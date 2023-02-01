4814b0146715b96f429adf17dd9db52ac4548182
Bram Gibson <Bram.gibson@hotmail.com>
2023-01-31 19:53:41 -0800

    got functionality working! working on readme, deployment to heroku and final touches.

---
+++ Develop/controllers/apiController.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
+let noteDb = require("../db/db.json");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+router.get("/", (req, res) => res.send(noteDb));
+
+router.post("/", (req, res) => {
+  const addNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send("woopsie");
+      throw err;
+    } else {
+      const noteData = JSON.parse(data);
+      console.log(noteData);
+
+      noteData.push(addNote);
+      fs.writeFile("./db/db.json", JSON.stringify(noteData, null, 4), (err) => {
+        if (err) {
+          res.status(500).send("woopsie!");
+          throw err;
+        } else {
+          res.send("data added");
+        }
+        res.send(noteData)
+      });
+    }
+  });
+});
+router.get("/:id", (req, res) => {
+  for (let i = 0; i < noteDb.length; i++) {
+    const note = noteDb[i];
+    if (note.id === req.params.id) {
+      return res.json(note);
+    }
+  }
+});
+router.delete("/:id", (req, res) => {
+  noteDb = noteDb.filter((note) => {
+    if (note.id == req.params.id) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+  fs.writeFile("./db/db.json", JSON.stringify(noteDb, null, 4), (err) => {
+    if (err) {
+      res.status(500).send("woopsie!");
+      throw err;
+    } else {
+      res.send("data deleted");
+    }
+    // console.log(noteData)
+    // res.send("delete request called")
+  });
+});
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -2,44 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || "3000"
 const path = require('path');
-const noteDb = require("./db/db.json")
-const uuid = require("uuid")
 const fs = require("fs")
+const apiRoutes = require('./controllers/apiController')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-// const allRoutes = require("./controllers");
-// app.use(allRoutes)
+app.use('/api/notes',apiRoutes)
+
 
 app.get('/notes',(req,res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('/api/notes',(req,res)=> res.send(noteDb))
-
-app.post('/api/notes',(req,res)=>{
-    fs.readFile("./db/db.json","utf-8",((err,data)=>{
-        if(err){
-            res.status(500).send("woopsie")
-            throw err
-        } else {
-            const noteData = JSON.parse(data);
-            noteData.push(req.body)
-            fs.writeFile("./db/db.json",JSON.stringify(noteData,null,4),(err)=>{
-                if (err){
-                    res.status(500).send("woopsie!")
-                    throw err
-                } else{
-                    res.send("data added")
-                }
-            })
-        }
-    }))
-    // console.log(req.body)
-    // noteDb.push(req.body)
-    res.send("post request received")
-})
 
 app.get('*',(req,res) =>
     res.sendFile(path.join(__dirname, '/public/index.html') ));
