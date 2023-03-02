2420ff01ed7e1892d1bdf742482b071c86ca0a03
Bram Gibson <Bram.gibson@hotmail.com>
2023-03-02 11:24:53 -0800

    bugfixing, now updates after saving. now working on heroku deployment

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -4,39 +4,26 @@ const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
 let noteDb = require("../db/db.json");
 const app = express();
+const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-router.get("/", (req, res) => res.send(noteDb));
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '../db/db.json'));
+});
 
 router.post("/", (req, res) => {
-  const addNote = {
-    id: uuidv4(),
-    title: req.body.title,
-    text: req.body.text,
+  let userNotes = fs.readFileSync('db/db.json');
+  userNotes = JSON.parse(userNotes);
+  res.json(userNotes);
+  let noteObj = {
+      id: uuidv4(),
+      title: req.body.title,
+      text: req.body.text,
   };
-
-  fs.readFile("./db/db.json", "utf-8", (err, data) => {
-    if (err) {
-      res.status(500).send("woopsie");
-      throw err;
-    } else {
-      const noteData = JSON.parse(data);
-      console.log(noteData);
-
-      noteData.push(addNote);
-      fs.writeFile("./db/db.json", JSON.stringify(noteData, null, 4), (err) => {
-        if (err) {
-          res.status(500).send("woopsie!");
-          throw err;
-        } else {
-          res.send(addNote)
-        }
-        res.send(noteData)
-      });
-    }
-    // res.send(noteData)
-  });
+  userNotes.push(noteObj);
+  fs.writeFileSync('db/db.json', JSON.stringify(userNotes));
+  // res.json(userNotes);
 });
 router.get("/:id", (req, res) => {
   for (let i = 0; i < noteDb.length; i++) {
